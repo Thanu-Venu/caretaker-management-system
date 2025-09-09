89f80242293dba68de1904bea425afdbb33a016d
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2025-09-09 15:27:33 +0530

    made changes in the caretaker sidebar

--- app/views/templates/caretaker/ct_sidebar.php
+++ app/views/templates/caretaker/ct_sidebar.php
@@ -12,90 +12,17 @@
     <br><br><br>
     <ul class="nav-links">
 
-      <li>
-        <a href="#">
-          <i class='bx bxs-dashboard'></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-calendar'></i>
-          <span class="link_name">My Schedule</span>
-          <i class='bx bx-chevron-down arrow'></i>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="#">Today’s Schedule</a></li>
-          <li><a href="#">Upcoming</a></li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-user-pin'></i>
-          <span class="link_name">Clients Assigned</span>
-          <i class='bx bx-chevron-down arrow'></i>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="#">View Clients</a></li>
-          <li><a href="#">Service Details</a></li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-calendar-check'></i>
-          <span class="link_name">Leave Request</span>
-          <i class='bx bx-chevron-down arrow'></i>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="#">Request Leave</a></li>
-          <li><a href="#">Leave History</a></li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-error'></i>
-          <span class="link_name">Complaints</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-message-dots'></i>
-          <span class="link_name">Feedback</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-report'></i>
-          <span class="link_name">Reports</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-user-circle'></i>
-          <span class="link_name">Profile</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bxs-cog'></i>
-          <span class="link_name">Settings</span>
-        </a>
-      </li>
-
-      <li class="logout">
-        <a href="#">
-          <i class='bx bx-log-out'></i>
-          <span class="link_name">Logout</span>
-        </a>
-      </li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_dashboard"><i class='bx bxs-dashboard'></i><span class="link_name">Dashboard</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_schedule"><i class='bx bxs-calendar'></i><span class="link_name">My Schedule</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_bookings"><i class='bx bx-book-alt'></i><span class="link_name">Bookings</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_clientsassigned"><i class='bx bxs-user-pin'></i><span class="link_name">Clients Assigned</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_leave"><i class='bx bxs-calendar-check'></i><span class="link_name">Leave Request</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_complaints"><i class='bx bxs-error'></i><span class="link_name">Complaints</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_feedback"><i class='bx bxs-message-dots'></i><span class="link_name">Feedback</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_reports"><i class='bx bxs-report'></i><span class="link_name">Reports</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_profile"><i class='bx bxs-user-circle'></i><span class="link_name">Profile</span></a></li>
+      <li><a href="http://localhost/CMA/public?url=caretaker/ct_settings"><i class='bx bxs-cog'></i><span class="link_name">Settings</span></a></li>
+      <li class="logout"><a href="http://localhost/CMA/public"><i class='bx bx-log-out'></i><span class="link_name">Logout</span></a></li>
 
     </ul>
   </div>
