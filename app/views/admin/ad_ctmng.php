4e1e643f1ac92ee09ad1f765f9a349d90915dc24
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2025-10-27 07:29:47 +0530

    created separate header file for admin

--- app/views/admin/ad_ctmng.php
+++ app/views/admin/ad_ctmng.php
@@ -1,4 +1,4 @@
-<?php  include_once APPROOT . "/views/templates/client/c_header.php"; ?>
+<?php  include_once APPROOT . "/views/templates/admin/ad_header.php"; ?>
 <?php  include_once APPROOT . "/views/templates/admin/ad_sidebar.php"; ?>
 
 
