3863a3ec11f2e5168dcae064bf60f15aeb831167
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2026-02-10 07:12:56 +0530

    made some changes

--- app/core/Database.php
+++ app/core/Database.php
@@ -5,14 +5,14 @@ class Database {
     public function __construct() {
 
 
-        $this->conn = new mysqli("localhost", "root", "", "smartcare");
+        $this->conn = new mysqli("localhost", "root", "Thanuvenu", "smartcare");
 
 
      
 
         $host = "localhost";
         $user = "root";
-        $pass = "";            
+        $pass = "Thanuvenu";            
         $dbname = "smartcare";  
 
         $this->conn = new mysqli($host, $user, $pass, $dbname);
