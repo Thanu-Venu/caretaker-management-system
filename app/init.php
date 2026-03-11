e0511092755ec6b6cac424f7463b68b94d377842
satheesan999 <rajasekarsatheesan2003@gmail.com>
2026-03-12 00:05:43 +0530

    Update the authcontroller function inside the init.php

--- app/init.php
+++ app/init.php
@@ -1,11 +1,11 @@
 <?php
 session_start();  // Start session for the whole app
 
-// Set default timezone to Sri Lanka
-date_default_timezone_set('Asia/Colombo');
-
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
 require_once 'config/config.php';
+require_once 'core/AuthSession.php';
+
+AuthSession::bootstrap();
 $app = new App();
