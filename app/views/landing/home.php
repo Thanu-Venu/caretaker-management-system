4f5d0461673daf01463b8bd89c70a9cc8bba8e7e
Piyula <annepiyula@gmail.com>
2026-04-04 02:26:35 +0530

    Development home page

--- app/views/landing/home.php
+++ app/views/landing/home.php
@@ -13,7 +13,7 @@
 
   <!-- Icons + Font -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/landing.css">
 
 </head>
