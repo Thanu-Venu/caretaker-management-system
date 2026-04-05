c32ea715dfe445a5cbe99e5ca6c3fbdb401cd8a2
satheesan999 <rajasekarsatheesan2003@gmail.com>
2026-04-05 18:01:23 +0530

    update the payhereconfig.php in the init.php

--- app/init.php
+++ app/init.php
@@ -5,6 +5,9 @@ require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
 require_once 'config/config.php';
+if (file_exists(__DIR__ . '/config/payhere_config.php')) {
+	require_once 'config/payhere_config.php';
+}
 require_once 'core/AuthSession.php';
 
 AuthSession::bootstrap();
